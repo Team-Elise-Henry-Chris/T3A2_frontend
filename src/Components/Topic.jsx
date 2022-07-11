546db2be732f432196492399773d1d7cd82684f4
chrismyhub <keyoch@gmail.com>
2022-07-11 16:11:54 +1000

    Changed class to className in Topic.jsx to clear browser console error

--- src/Components/Topic.jsx
+++ src/Components/Topic.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Topic = ({ topic }) => {
     return (
-        <div class="card w-96 bg-base-100 shadow-xl image-full m-4">
+        <div className="card w-96 bg-base-100 shadow-xl image-full m-4">
             <figure><img src="https://placeimg.com/400/225/arch" alt="" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">{topic.name}</h2>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary"><Link to={`/topic/${topic._id}`}>View Posts</Link></button>
+            <div className="card-body">
+                <h2 className="card-title">{topic.name}</h2>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary"><Link to={`/topic/${topic._id}`}>View Posts</Link></button>
                 </div>
             </div>
         </div>
