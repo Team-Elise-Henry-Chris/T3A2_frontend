51dfabcaf7789183a6c401b46f7fec28d402d97f
chrismyhub <keyoch@gmail.com>
2022-07-12 11:20:06 +1000

    Reconfigured Nav.jsx so that Sign-in/Sign-up shows in dropdown list when in mobile view.

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,17 +12,18 @@ const Nav = () => {
                         <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="#">Create Post</Link></li>
+                            <li><Link to="#">Sign In/Sign Up</Link></li>
                         </ul>
                     </div>
                     <button className="btn btn-ghost normal-case text-xl">Teach Yo'Self</button>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-3">
+                    <ul className="menu menu-horizontal p-5">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="#">Create Post</Link></li>
                     </ul>
                 </div>
-                <div className="navbar-end">
+                <div className="navbar-end hidden lg:flex">
                     <button className="btn">Sign In/Sign Up</button>
                 </div>
             </div>
