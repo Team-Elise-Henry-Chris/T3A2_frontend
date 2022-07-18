17e7bf2dd3dcf72b1fd908fd2bd32859ed119e4c
Elise Hall <elisemaree28@gmail.com>
2022-07-18 14:55:55 +1000

    Added logic for signin authentication

--- src/Components/Pages/SignIn.jsx
+++ src/Components/Pages/SignIn.jsx
@@ -1,69 +1,109 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useRef, useEffect, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import AuthContext from '../AuthProvider'
+import axios from '../../api/axios'
+import Alert from '../Alert'
+
+const LOGIN_URL = '/api/v1/user'
 
 const SignIn = () => {
+    const { setAuth } = useContext(AuthContext)
+    const userRef = useRef()
+    const nav = useNavigate()
+
     const [signInInfo, setSignInInfo] = useState({
         email: '',
         password: '',
     })
 
+    const [showError, setShowError] = useState(false)
+
+    // TODO: Handle setting Auth
+
     const handleChange = (event) => {
         setSignInInfo({ ...signInInfo, [event.target.name]: event.target.value })
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        
-        // TODO: Axios call to backend
+
+        try {
+            const response = await axios.put(LOGIN_URL, signInInfo)
+            console.log(response)
+            setSignInInfo({ email: '', password: '' })
+            const accessToken = response.data?.accessToken
+            const role = response.data?.role
+            setAuth({ email: signInInfo.email, role, accessToken })
+
+            nav('/')
+
+        } catch (err) {
+            setShowError(true)
+            setSignInInfo({ ...signInInfo, password: '' })
+        }
+
     }
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+    }, [signInInfo.email, signInInfo.password])
 
 
     return (
-        <div className="relative flex flex-grow p-3">
-            <form onSubmit={handleSubmit} className="hero bg-base-200 border rounded ">
-                <div className="hero-content flex-col w-full">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign In!</h1>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    placeholder="email"
-                                    className="input input-bordered"
-                                    name="email"
-                                    value={signInInfo.email}
-                                    onChange={handleChange}                                    
-                                />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input
-                                    type="password"
-                                    placeholder="password"
-                                    className="input input-bordered"
-                                    name="password"
-                                    value={signInInfo.password}
-                                    onChange={handleChange} 
-                                />
-                                <label className="label">
-                                    <Link to="/create-account" className="label-text-alt link link-hover">Don't have an account? Sign up now!</Link>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Sign In</button>
+        <>
+            <div className="relative flex flex-grow p-3">
+                <form onSubmit={handleSubmit} className="hero bg-base-200 border rounded ">
+                    <div className="hero-content flex-col w-full">
+                        <div className="text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">Sign In!</h1>
+                        </div>
+                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <div className="card-body">
+                                {showError && <Alert status="error" message="Invalid email or password" />}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input
+                                        ref={userRef}
+                                        type="text"
+                                        placeholder="email"
+                                        className="input input-bordered"
+                                        name="email"
+                                        value={signInInfo.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input
+                                        type="password"
+                                        placeholder="password"
+                                        className="input input-bordered"
+                                        name="password"
+                                        value={signInInfo.password}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                    <label className="label">
+                                        <Link to="/create-account" className="label-text-alt link link-hover">Don't have an account? Sign up now!</Link>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Sign In</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        </>
     )
 }
 
