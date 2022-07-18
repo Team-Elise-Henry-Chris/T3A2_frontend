3b80e75c7fd9d438570172d810f018ebb0205433
Elise Hall <elisemaree28@gmail.com>
2022-07-18 17:21:32 +1000

    Updated axios api helper

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const endpoint = import.meta.env.API_ENDPOINT ?? process.env.API_ENDPOINT
+
 export default axios.create({
-    baseURL: import.meta.env.API_ENDPOINT || 'http://localhost:4000'
+    baseURL: endpoint || 'http://localhost:4000'
 })
